2d6de3763427bc108b8fb9d9ef827d30eca74fb3
Veaceslav Babin <babinslava@gmail.com>
2021-03-27 07:34:30 +0200

    Add audio context. Preload audio files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+// for legacy browsers
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+const audioContext = new AudioContext();
+
 sounds = ["fart", "whip", "cricket", "bell", "gun", "duck", "laughter", "kiss"];
 sounds.forEach((sound) => {
+  audio = new Audio("sounds/" + sound + ".mp3");
   document.getElementById(sound).addEventListener("click", playSound);
 
   function playSound() {
