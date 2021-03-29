24c3229e7b99074281a8d58c5fa43cdb8f1d4085
Veaceslav Babin <babinslava@gmail.com>
2021-03-29 22:22:04 +0300

    Make use of audio context

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 // for legacy browsers
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 
-const audioContext = new AudioContext();
+let audioContext;
 
 sounds = ["fart", "burp", "whip", "cricket", "bell", "gun", "duck", "laughter", "kiss"];
+audios = {};
+tracks = {};
+
 sounds.forEach((sound) => {
-  audio = new Audio("sounds/" + sound + ".mp3");
+  audios[sound] = new Audio("sounds/" + sound + ".mp3");
   document.getElementById(sound).addEventListener("click", playSound);
 
   function playSound() {
-    audio = new Audio("sounds/" + sound + ".mp3");
+    audio = audios[sound];
+    audioContext = audioContext || new AudioContext();
+    if (!tracks[sound]) {
+      const track = audioContext.createMediaElementSource(audio);
+      track.connect(audioContext.destination);
+      tracks[sound] = track;
+    }
     audio.play();
   }
 });
