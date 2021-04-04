419eac1ed267350cd5062a715b8e67dedfe39964
Veaceslav Babin <babinslava@gmail.com>
2021-04-04 14:55:50 +0300

    Make service worker easier to read

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("js/sw.js");
+  navigator.serviceWorker.register("sw.js");
 }
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
---
+++ sw.js
@@ -0,0 +1,9 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js");
+
+const {registerRoute} = workbox.routing;
+const {StaleWhileRevalidate} = workbox.strategies;
+
+registerRoute(({url}) => url.pathname.startsWith("/sounds/"), new StaleWhileRevalidate());
+registerRoute(({url}) => url.pathname.startsWith("/images/"), new StaleWhileRevalidate());
+registerRoute(({url}) => url.pathname.startsWith("/js/"), new StaleWhileRevalidate());
+registerRoute(({url}) => url.pathname.startsWith("/css/"), new StaleWhileRevalidate());
