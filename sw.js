ee70667ba3f1ff4f95cc7791c799fbe94f792bad
Veaceslav Babin <babinslava@gmail.com>
2021-04-04 15:15:27 +0300

    Cache index page

--- sw.js
+++ sw.js
@@ -2,8 +2,12 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox
 
 const {registerRoute} = workbox.routing;
 const {StaleWhileRevalidate} = workbox.strategies;
+const {skipWaiting} = workbox.core;
 
 registerRoute(({url}) => url.pathname.startsWith("/sounds/"), new StaleWhileRevalidate());
 registerRoute(({url}) => url.pathname.startsWith("/images/"), new StaleWhileRevalidate());
 registerRoute(({url}) => url.pathname.startsWith("/js/"), new StaleWhileRevalidate());
 registerRoute(({url}) => url.pathname.startsWith("/css/"), new StaleWhileRevalidate());
+registerRoute(({url}) => url.pathname === "/", new StaleWhileRevalidate());
+
+skipWaiting();
