a5c60763daf793abed4d9e1ba15bc399aab3203c
Veaceslav Babin <babinslava@gmail.com>
2021-04-04 15:38:24 +0300

    skipWaiting update

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox
 
 const {registerRoute} = workbox.routing;
 const {StaleWhileRevalidate} = workbox.strategies;
-const {skipWaiting} = workbox.core;
 
 registerRoute(({url}) => url.pathname.startsWith("/sounds/"), new StaleWhileRevalidate());
 registerRoute(({url}) => url.pathname.startsWith("/images/"), new StaleWhileRevalidate());
@@ -10,4 +9,4 @@ registerRoute(({url}) => url.pathname.startsWith("/js/"), new StaleWhileRevalida
 registerRoute(({url}) => url.pathname.startsWith("/css/"), new StaleWhileRevalidate());
 registerRoute(({url}) => url.pathname === "/", new StaleWhileRevalidate());
 
-skipWaiting();
+self.skipWaiting();
